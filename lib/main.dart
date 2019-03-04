c617bae70751bb4839d550bc1f809b9f5a787f77
严俊东 <1172482914@qq.com>
2019-03-04 18:31:11 +0800

    数据获取

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,12 @@
 import 'package:flutter/material.dart';
 import './widget/HomePage.dart';
+import 'package:flutter/rendering.dart';
 
-void main() => runApp(MyApp());
+
+void main() {
+  debugPaintSizeEnabled = true;
+  runApp(MyApp());
+}
 
 class MyApp extends StatelessWidget {
   // This widget is the root of your application.
--- lib/widget/HomePage.dart
+++ lib/widget/HomePage.dart
@@ -1,10 +1,56 @@
 import 'package:flutter/material.dart';
-
+import 'package:http/http.dart' as http;
+import 'dart:convert' as convert;
 class HomePage extends StatefulWidget {
   _HomePage createState() => _HomePage();
 }
 
 class _HomePage extends State<HomePage> {
+  List _data = [];
+
+  _getList() async {
+    var url =
+        "https://movie.douban.com/j/search_subjects?type=movie&tag=%E7%83%AD%E9%97%A8&page_limit=50&page_start=0";
+    var response = await http.get(url);
+    if (response.statusCode == 200) {
+      var jsonResponse = convert.jsonDecode(response.body);
+      this.setState(() {
+        this._data = jsonResponse['subjects'];
+      });
+      print("Number of books about http: $jsonResponse.");
+    } else {
+      print("Request failed with status: ${response.statusCode}.");
+    }
+  }
+
+  _getStars(String rateStr) {
+    var rate =  double.parse(rateStr);
+    List<Widget> starWidget = [];
+    var  starts = (rate / 2).floor();
+    for(var i = 0 ; i < 5; i ++) {
+      var color;
+      if (i < starts) {
+        color = 0xffffb712;
+      } else {
+        color = 0xffe5e5e5;
+      }
+      starWidget.add(Expanded(
+          child: Icon(Icons.star, color: Color(color)),
+        ));
+    }
+
+    return Row(
+      mainAxisSize: MainAxisSize.min,
+      children: starWidget,
+    );
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    this._getList();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -29,20 +75,53 @@ class _HomePage extends State<HomePage> {
           ),
           SliverGrid(
             gridDelegate: SliverGridDelegateWithMaxCrossAxisExtent(
-              maxCrossAxisExtent: 200.0,
-              mainAxisSpacing: 10.0,
-              crossAxisSpacing: 10.0,
-              childAspectRatio: 4.0,
+              maxCrossAxisExtent: 125.0,
+              // mainAxisSpacing: 10.0,
+              // crossAxisSpacing: 10.0,
+              childAspectRatio: .7,
             ),
             delegate: SliverChildBuilderDelegate(
               (BuildContext context, int index) {
+                var item = this._data[index];
                 return Container(
                   alignment: Alignment.center,
-                  color: Colors.teal[100 * (index % 9)],
-                  child: Text('grid item $index'),
+                  child: Column(
+                    children: <Widget>[
+                      Expanded(
+                        flex: 3,
+                        child: Center(
+                          child: Image.network(item['cover']),
+                        ),
+                      ),
+                      Expanded(
+                          child: Column(
+                        children: <Widget>[
+                          Expanded(
+                            child: Center(
+                                child: Text(
+                              item['title'],
+                              style: TextStyle(fontSize: 12),
+                            )),
+                          ),
+                          Expanded(
+                            child: Row(
+                              children: <Widget>[
+                                Expanded(
+                                  child: this._getStars(item['rate'])
+                                ),
+                                Expanded(
+                                  child: Text(item['rate']),
+                                )
+                              ],
+                            ),
+                          )
+                        ],
+                      ))
+                    ],
+                  ),
                 );
               },
-              childCount: 20,
+              childCount: this._data.length,
             ),
           ),
         ],
